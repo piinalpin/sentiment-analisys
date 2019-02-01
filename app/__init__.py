b21bdece819df7f2c4555e0d26bc7ece2884f396
Alvinditya Saputra <alvindityas@gmail.com>
2019-02-01 16:02:39 +0700

    Change init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
-app = Flask (__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from app.modul.views import upload
-from app.modul.views import upload_hasil
-from app.modul.views import Index
-from app.modul.views import hasil
-from app.modul.views import Grafik
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "db/sentiment_analisys.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.modul.views import *
